ed98a182af7a6b6a33a274c728a081792949625d
Phillip Dunham <pdunham@gmail.com>
2013-01-19 15:36:07 -0600

    tweaks to ScrobbleTweeter and TweeterCOntroller. And added some attributes to User.

--- src/scrobblefilter/web/TweeterController.java
+++ src/scrobblefilter/web/TweeterController.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 import org.springframework.web.bind.annotation.RequestMapping;
