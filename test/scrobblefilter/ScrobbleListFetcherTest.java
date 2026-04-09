132007e93bf707cbb8cad1f14d7dca600c944015
Phillip Dunham <pdunham@gmail.com>
2026-04-09 11:39:44 -0500

    fixed network test

--- test/scrobblefilter/ScrobbleListFetcherTest.java
+++ test/scrobblefilter/ScrobbleListFetcherTest.java
@@ -2,13 +2,15 @@ package scrobblefilter;
 
 import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Iterator;
-
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 
 import scrobblefilter.net.ScrobbleListFetcher;
@@ -17,42 +19,35 @@ import scrobblefilter.net.impl.NetworkedScrobbleListFetcher;
 
 public class ScrobbleListFetcherTest {
 
-	@Test
-	public void test() throws JsonParseException, JsonMappingException, IOException {
-		
-		ScrobbleListFetcher fetcher = new FakeScrobbleListFetcher();
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode rootNode = mapper.readValue(fetcher.fetchList("pdunham"), JsonNode.class);
-		JsonNode topartistNode = rootNode.get("topartists");
-		JsonNode artistNode = topartistNode.get("artist");
-		Iterator<JsonNode> artists = artistNode.getElements();
-		while (artists.hasNext()) {
-			
-			JsonNode artist = artists.next();
-			Iterator<String> fieldNames = artist.getFieldNames();
-			assertEquals("name",fieldNames.next());
-			System.out.println( artist.get("name") + ": "+artist.get("playcount").asInt() + " plays");
-			
+	@Before
+	public void setUp() throws IOException {
+		try (InputStream in = new FileInputStream("war/WEB-INF/scrobblefilter.properties")) {
+			AppConfig.load(in);
 		}
 	}
-	
+
+	@Test
+	public void testFakeScrobbleFetcher() throws JsonParseException, JsonMappingException, IOException {
+		assertArtistListStructure(new FakeScrobbleListFetcher());
+	}
+
 	@Test
 	public void testNetworkScrobbleFetcher() throws JsonParseException, JsonMappingException, IOException {
-		ScrobbleListFetcher fetcher = new NetworkedScrobbleListFetcher();
+		assertArtistListStructure(new NetworkedScrobbleListFetcher());
+	}
+
+	private void assertArtistListStructure(ScrobbleListFetcher fetcher)
+			throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
-		JsonNode rootNode = mapper.readValue(fetcher.fetchList("pdunham"), JsonNode.class)	;
-		JsonNode topartistNode = rootNode.get("topartists");
-		JsonNode artistNode = topartistNode.get("artist");
+		JsonNode rootNode = mapper.readValue(fetcher.fetchList("pdunham"), JsonNode.class);
+		JsonNode artistNode = rootNode.get("topartists").get("artist");
 		Iterator<JsonNode> artists = artistNode.getElements();
 		while (artists.hasNext()) {
-			
 			JsonNode artist = artists.next();
-			Iterator<String> fieldNames = artist.getFieldNames();
-			assertEquals("name",fieldNames.next());
-			System.out.println( artist.get("name") + ": "+artist.get("playcount").asInt() + " plays");
-			
+			assertNotNull("Artist missing 'name' field", artist.get("name"));
+			assertNotNull("Artist missing 'playcount' field", artist.get("playcount"));
+			System.out.println(artist.get("name") + ": " + artist.get("playcount").asInt() + " plays");
 		}
-		
 	}
 
 }
